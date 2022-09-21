59b02ccca02fe19710ae841e0d7baff050b3b198
vanvans03-def <teerahitchana@gmail.com>
2022-09-21 10:18:58 +0700

    week6 work5

--- work4/index.php
+++ work4/index.php
@@ -1,37 +1,90 @@
+<?php
+session_start();
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>First Page</title>
     <style>
-       .loginLink{
+    .loginLink {
         float: right;
-       }
+
+    }
     </style>
 </head>
+
 <body>
     <form>
-    <h1>Teera Webboard</h1>
-    <hr>
-    <p>หมวดหมู่
-        <select name="option">
-            <option value="1">--ทั้งหมด--</option>
-            <option value="2">เรื่องทั่วไป</option>
-            <option value="3">เรื่องเรียน</option>
-        </select>
-        <span class="loginLink"><a href="login.html">เข้าสู่ระบบ</a></span>
-    </p>
-    <ul>
-        <?php
+        <h1>Teera Webboard</h1>
+        <hr>
+        <p>หมวดหมู่
+            <select name="option">
+                <option value="1">--ทั้งหมด--</option>
+                <option value="2">เรื่องทั่วไป</option>
+                <option value="3">เรื่องเรียน</option>
+            </select>
+            </p>
+            <?php 
+        if(empty($_SESSION['username'])){
+        $Login=FALSE;
+        }else{
+        $Login=TRUE;
+        }
+
+
+        if($Login == TRUE){
+
+            echo "ID = ".$_SESSION['id']."";
+           
+            ?>
+            <span class="" style="margin-left: 30px;">ผู้ใช้งานระบบ: <?php  echo $_SESSION['username']?></span>
+            <span class="loginLink"><a href="logout.php">ออกจากระบบ</a></span>
+            <?php
+        }elseif($Login == FALSE){
+            ?>
+            <span class="" style="margin-left: 30px;">ผู้ใช้งานระบบ: GUEST</span>
+            <span class="loginLink"><a href="login.php">เข้าสู่ระบบ</a></span>
+            <?php
+        }
+        ?>
+
+
+
+       
+        <a href="newpost.php" class="">สร้างกระทู้ใหม่</a>
+        <ul>
+
+            <?php
         for ($i = 1 ; $i <= 10 ; $i++){
             ?>
-        <li><a href="post.php?id=<?php echo $i?>" >กระทู้ที่ <?php echo $i?></a></li>
-      <?php 
+            <li><a href="post.php?id=<?php echo $i?>">กระทู้ที่ <?php echo $i?></a>
+
+                <?php 
+                if(empty($_SESSION['role'])){
+                   
+                }else{
+
+                    if($_SESSION['role'] == 'a'){
+                            ?>
+                            <a href="delete.php?id=<?php echo $i?>" class="">ลบ</a>
+                    <?php
+                    }
+                }
+        ?>
+            </li>
+
+            <?php 
        }
        ?>
-    </ul>
-</form>
+
+        </ul>
+    </form>
 </body>
+
 </html>
